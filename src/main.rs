2e21a6f64cde02c6b80f3d7588965bc2bdeff165
akhercha <adelkherchache@gmail.com>
2023-08-27 00:07:27 +0200

    playing song

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use raylib::prelude::*;
 
-fn main() {
-    let (mut rl, thread) = raylib::init().size(860, 600).title("Hello, World").build();
+use raylib::core::audio::{Music, RaylibAudio};
 
+const SCREEN_WIDTH: i32 = 860;
+const SCREEN_HEIGHT: i32 = 600;
+const MUSIC_NIGHTCORE: &str = "songs/light-it-up-nightcore.mp3";
+
+fn main() {
+    let (mut rl, thread) = raylib::init()
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("DJust")
+        .build();
+    let mut ra: RaylibAudio = RaylibAudio::init_audio_device();
+    let mut music = Music::load_music_stream(&thread, MUSIC_NIGHTCORE).unwrap();
+    ra.play_music_stream(&mut music);
+    rl.set_target_fps(60);
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
-
         d.clear_background(Color::RED);
-        d.draw_text("Hello, world!", 430, 300, 20, Color::BLACK);
+        ra.update_music_stream(&mut music);
     }
 }
