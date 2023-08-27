399d9129498c2a619f0a56ee103743d059f67965
akhercha <adelkherchache@gmail.com>
2023-08-27 13:57:34 +0200

    switched to i16 + new songs to OGG

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,21 @@ use raylib::core::audio::{Music, RaylibAudio};
 use raylib::core::color::Color;
 use raylib::prelude::*;
 
-const MUSIC: &str = "songs/headie-one-back-to-basics.ogg";
-const BUFFER_SIZE: usize = 2048;
+// const MUSIC: &str = "songs/headie-one-back-to-basics.ogg";
+const MUSIC: &str = "songs/crankdat-higher.ogg";
+// const MUSIC: &str = "songs/boom-boom-japan.ogg";
+const BUFFER_SIZE: usize = 4096;
 
-fn get_samples_buffer(music: &Music) -> Vec<i32> {
-    const HEADER_SIZE: usize = 87;
+fn get_samples_buffer(music: &Music) -> Vec<i16> {
+    const HEADER_SIZE: usize = 128 + 64;
     let buffer_size = BUFFER_SIZE + HEADER_SIZE;
-    let mut my_buffer: Vec<i32> = vec![0; buffer_size];
+    let mut my_buffer: Vec<i16> = vec![0; buffer_size];
     if music.stream.buffer.is_null() {
         panic!("Buffer is null!");
     }
     unsafe {
         my_buffer.copy_from_slice(std::slice::from_raw_parts(
-            music.stream.buffer as *const i32,
+            music.stream.buffer as *const i16,
             buffer_size,
         ));
     }
@@ -47,7 +49,7 @@ fn main() {
                 let sample: f32 = *v as f32;
                 if sample > 0.0 {
                     // Normalize values to fit screen
-                    let t = sample / i32::MAX as f32;
+                    let t = sample / i16::MAX as f32;
                     let v_pos = Vector2 {
                         x: cell_w * i as f32,
                         y: h / 2.0 - h / 2.0 * t,
@@ -58,7 +60,7 @@ fn main() {
                     };
                     d.draw_rectangle_v(v_pos, v_size, Color::new(245, 85, 73, 255));
                 } else {
-                    let t: f32 = sample / i32::MIN as f32;
+                    let t: f32 = sample / i16::MIN as f32;
                     let v_pos = Vector2 {
                         x: cell_w * i as f32,
                         y: h / 2.0,
