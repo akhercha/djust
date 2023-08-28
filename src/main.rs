401af03619cc00949b84efc9aad43fcf6ecb9e54
akhercha <adelkherchache@gmail.com>
2023-08-28 08:58:39 +0200

    c way

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,82 @@ use raylib::consts::KeyboardKey::KEY_SPACE;
 use raylib::core::audio::{Music, RaylibAudio};
 use raylib::core::color::Color;
 use raylib::prelude::*;
+use std::os::raw::c_void;
 
-// const MUSIC: &str = "songs/headie-one-back-to-basics.ogg";
-const MUSIC: &str = "songs/crankdat-higher.ogg";
-// const MUSIC: &str = "songs/boom-boom-japan.ogg";
-// Full magic numbers :)
-const BUFFER_SIZE: usize = 6144 - 256; // 256 at the end, some sort of header?
+// Path to your music
+// const MUSIC: &str = "songs/dreams.ogg";
+const MUSIC: &str = "songs/headie-one-back-to-basics.ogg";
+// const MUSIC: &str = "songs/crankdat-higher.ogg";
 
-fn get_samples_buffer(music: &Music) -> Vec<i16> {
-    const HEADER_SIZE: usize = 128 + 64;
-    let buffer_size = BUFFER_SIZE + HEADER_SIZE;
-    let mut my_buffer: Vec<i16> = vec![0; buffer_size];
-    if music.stream.buffer.is_null() {
-        panic!("Buffer is null!");
+#[derive(Clone, Copy)]
+struct Frame {
+    left: f32,
+    right: f32,
+}
+
+const BUFFER_CAPACITY: usize = 4800;
+static mut GLOBAL_FRAMES: [Frame; BUFFER_CAPACITY] = [Frame {
+    left: 0.0,
+    right: 0.0,
+}; BUFFER_CAPACITY];
+static mut GLOBAL_FRAMES_COUNT: usize = 0;
+
+const COLOR_PALE_RED: Color = Color::new(245, 85, 73, 255);
+
+unsafe extern "C" fn callback(buffer_data: *mut c_void, frames: u32) {
+    let buffer_data = buffer_data as *mut Frame;
+    if frames as usize <= BUFFER_CAPACITY - GLOBAL_FRAMES_COUNT {
+        std::ptr::copy(
+            buffer_data,
+            GLOBAL_FRAMES
+                .as_mut_ptr()
+                .offset(GLOBAL_FRAMES_COUNT as isize),
+            frames as usize,
+        );
+        GLOBAL_FRAMES_COUNT += frames as usize;
+    } else if frames as usize <= BUFFER_CAPACITY {
+        std::ptr::copy(
+            GLOBAL_FRAMES.as_mut_ptr().offset(frames as isize),
+            GLOBAL_FRAMES.as_mut_ptr(),
+            BUFFER_CAPACITY - frames as usize,
+        );
+        std::ptr::copy(
+            buffer_data,
+            GLOBAL_FRAMES
+                .as_mut_ptr()
+                .offset((BUFFER_CAPACITY - frames as usize) as isize),
+            frames as usize,
+        );
+    } else {
+        std::ptr::copy(buffer_data, GLOBAL_FRAMES.as_mut_ptr(), BUFFER_CAPACITY);
+        GLOBAL_FRAMES_COUNT = BUFFER_CAPACITY;
     }
-    unsafe {
-        my_buffer.copy_from_slice(std::slice::from_raw_parts(
-            music.stream.buffer as *const i16,
-            buffer_size,
-        ));
+}
+
+fn get_drawable_vecs(sample: f32, cell_w: f32, h: f32, i: f32) -> (Vector2, Vector2) {
+    if sample > 0.0 {
+        (
+            Vector2 {
+                x: cell_w * i,
+                y: h / 2.0 - (h / 2.0 * sample),
+            },
+            Vector2 {
+                x: cell_w,
+                y: (h / 2.0 * sample),
+            },
+        )
+    } else {
+        (
+            Vector2 {
+                x: cell_w * i,
+                y: h / 2.0,
+            },
+            Vector2 {
+                x: cell_w,
+                y: (h / 2.0 * sample.abs()),
+            },
+        )
     }
-    my_buffer.drain(0..HEADER_SIZE);
-    my_buffer
 }
 
 fn main() {
@@ -32,50 +86,29 @@ fn main() {
     let mut ra: RaylibAudio = RaylibAudio::init_audio_device();
     let mut music = Music::load_music_stream(&thread, MUSIC).unwrap();
     ra.play_music_stream(&mut music);
-    rl.set_target_fps(60);
+    unsafe {
+        ffi::AttachAudioStreamProcessor(music.stream, Some(callback));
+    }
 
+    rl.set_target_fps(60);
     while !rl.window_should_close() {
-        // draws
         {
             let mut d = rl.begin_drawing(&thread);
             d.clear_background(Color::new(12, 12, 13, 255));
-
-            let my_buffer = get_samples_buffer(&music);
-
             let h = d.get_screen_height() as f32;
             let w = d.get_screen_width() as f32;
-            let cell_w: f32 = w / my_buffer.len() as f32;
-
-            for (i, v) in my_buffer.iter().enumerate() {
-                let sample: f32 = *v as f32;
-                if sample > 0.0 {
-                    // Normalize values to fit screen
-                    let t = sample / i16::MAX as f32;
-                    let v_pos = Vector2 {
-                        x: cell_w * i as f32,
-                        y: h / 2.0 - h / 2.0 * t,
-                    };
-                    let v_size = Vector2 {
-                        x: cell_w,
-                        y: h / 2.0 * t,
-                    };
-                    d.draw_rectangle_v(v_pos, v_size, Color::new(245, 85, 73, 255));
-                } else {
-                    let t: f32 = sample / i16::MIN as f32;
-                    let v_pos = Vector2 {
-                        x: cell_w * i as f32,
-                        y: h / 2.0,
-                    };
-                    let v_size = Vector2 {
-                        x: cell_w,
-                        y: h / 2.0 * t,
-                    };
-                    d.draw_rectangle_v(v_pos, v_size, Color::new(245, 85, 73, 255));
+            unsafe {
+                let cell_w: f32 = w / GLOBAL_FRAMES.len() as f32;
+                for (i, frame) in GLOBAL_FRAMES.iter().enumerate() {
+                    let (v_pos_left, v_size_left) =
+                        get_drawable_vecs(frame.left, cell_w, h, i as f32);
+                    let (v_pos_right, v_size_right) =
+                        get_drawable_vecs(frame.right, cell_w, h, i as f32);
+                    d.draw_rectangle_v(v_pos_left, v_size_left, COLOR_PALE_RED);
+                    d.draw_rectangle_v(v_pos_right, v_size_right, COLOR_PALE_RED);
                 }
             }
         }
-
-        // event loop
         {
             if rl.is_key_pressed(KEY_SPACE) {
                 if ra.is_music_stream_playing(&music) {
@@ -85,8 +118,6 @@ fn main() {
                 }
             }
         }
-
-        // music stream
         {
             ra.update_music_stream(&mut music);
         }
